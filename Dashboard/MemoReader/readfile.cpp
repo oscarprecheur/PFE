494a6d8de00960e6df0b18e8f23030545af803fe
oscarprecheur <oscar.precheur@gmail.com>
2020-12-15 14:30:44 +0100

    Suppression premiers caractère URL FIle

--- Dashboard/MemoReader/readfile.cpp
+++ Dashboard/MemoReader/readfile.cpp
@@ -9,18 +9,30 @@ readfile::readfile(QObject *parent):QObject(parent)
 
 void readfile::setFileUrl(QString NewFileUrl)
 {
+    if (NewFileUrl.contains("file:///"))
+            NewFileUrl.replace("file:///","");
+    qDebug()<<"Debug SetFileUrl1"<<NewFileUrl;
     FileUrl=NewFileUrl;
-    qDebug()<<"Debug SetFileUrl"<<FileUrl;
+    qDebug()<<"Debug SetFileUrl2"<<FileUrl;
+
+    readFileLine(4);
 }
 
 void readfile::readFileLine(int nLine)
 {
-    if (FileDir.exists(FileUrl))
-    {
-        qDebug()<<"Ce dossier existe";
-    }
-    else
-    {
-        qDebug()<<"Ce dossier n'existe pas ";
-    }
+
+//    if (FileDir.exists(FileUrl))
+//    {
+//        qDebug()<<"Ce dossier existe";
+//    }
+//    else
+//    {
+//        qDebug()<<"Ce dossier n'existe pas ";
+//    }
+
+    memoFile.setFileName(FileUrl);
+   if (!memoFile.open(QIODevice::ReadOnly | QIODevice::Text))
+       qDebug()<<"ok";
+   else
+       qDebug()<<"opened";
 }
