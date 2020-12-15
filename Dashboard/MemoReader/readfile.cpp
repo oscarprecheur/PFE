d290ed7e2d4649234bf53dc2f7c9e42fe60d7c54
oscarprecheur <oscar.precheur@gmail.com>
2020-12-15 16:42:52 +0100

    lecture de la date

--- Dashboard/MemoReader/readfile.cpp
+++ Dashboard/MemoReader/readfile.cpp
@@ -7,6 +7,21 @@ readfile::readfile(QObject *parent):QObject(parent)
 
 }
 
+bool readfile::openFile()
+{
+    memoFile.setFileName(FileUrl);
+   if (!memoFile.open(QIODevice::ReadOnly | QIODevice::Text))
+   {
+       qDebug()<<"File"<<FileUrl<<"Not Opened";
+       return false;
+   }
+   else
+   {
+       qDebug()<<"File"<<FileUrl<<" Opened";
+       return true;
+   }
+}
+
 void readfile::setFileUrl(QString NewFileUrl)
 {
     if (NewFileUrl.contains("file:///"))
@@ -15,14 +30,35 @@ void readfile::setFileUrl(QString NewFileUrl)
     FileUrl=NewFileUrl;
     qDebug()<<"Debug SetFileUrl2"<<FileUrl;
 
-    readFileLine(4);
+    initReading();
 }
 
-void readfile::readFileLine(int nLine)
+void readfile::initReading()
 {
-    memoFile.setFileName(FileUrl);
-   if (!memoFile.open(QIODevice::ReadOnly | QIODevice::Text))
-       qDebug()<<"File"<<FileUrl<<"Not Opened";
-   else
-       qDebug()<<"File"<<FileUrl<<" Opened";
+
+    openFile();
+    QString line;
+    QStringList listLineValue;
+
+    //date
+    line = memoFile.readLine();
+    listLineValue = line.split(";");
+    fileDate.setDate(listLineValue.at(3).toInt(),listLineValue.at(2).toInt(),listLineValue.at(1).toInt());
+    qDebug()<<fileDate;
+}
+
+void readfile::readFileLine(int nLine, int nColomn)
+{
+    openFile();
+    QString finalVal;
+    QString line;
+    QStringList listeDeMots;
+    for(int cpt=0;cpt<=nLine;cpt++)
+    {
+        line = memoFile.readLine();
+        listeDeMots = line.split(";");
+    }
+    finalVal=listeDeMots.at(nColomn);
+    qDebug()<<finalVal;
+
 }
--- Dashboard/MemoReader/readfile.h
+++ Dashboard/MemoReader/readfile.h
@@ -2,6 +2,8 @@
 #define READFILE_H
 
 #include <QFileDialog>
+#include <QDate>
+#include <QTime>
 
 
 class readfile: public QObject
@@ -13,7 +15,8 @@ public:
     readfile(QObject *parent = nullptr);
 
      Q_INVOKABLE void setFileUrl(QString NewFileUrl);
-     Q_INVOKABLE void readFileLine(int nLine);
+     Q_INVOKABLE void readFileLine(int, int);
+     Q_INVOKABLE void initReading();
 
 public slots:
 
@@ -23,6 +26,46 @@ private:
     QDir FileDir;
     QFile memoFile;
     int valtest;
+
+    bool openFile();
+
+    //valeurs données
+
+    //date
+    QDate fileDate;
+    //heure
+    QTime fileTime;
+    //tempo
+    float memoTempo;
+    //seuils tang
+    float tangageSeuil;
+    //seuils gite
+    float giteSeuil;
+    //temps
+    float time;
+    //distance
+    float distance;
+    //tangage
+    float tangageVal;
+    //tend tangage
+    float tangageTend;
+    //gite
+    float giteVal;
+    //tend gite
+    float giteTend;
+    //vitesse
+    float vitesseVal;
+    //tend vitesse
+    float vitesseTend;
+
+    //-------------à faire-----------
+    //vitesse moy 500
+    //vitesse moy 10 sec
+    //direction
+    //latitude
+    //longitude
+    //-------------------------------
+
 };
 
 #endif // READFILE_H
