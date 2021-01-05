16269a3de00fdac7af2af5f1f5166e2a1c56102a
oscarprecheur <oscar.precheur@gmail.com>
2021-01-05 18:52:17 +0100

    ajout test calc moy

--- Dashboard/Nacra17Dashboard/main.cpp
+++ Dashboard/Nacra17Dashboard/main.cpp
@@ -1,22 +1,41 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlComponent>
+#include <QTextStream>
 
 #include "valcapt.h"
+#include "valmoy.h"
 
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QGuiApplication app(argc, argv);
+        QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+        QGuiApplication app(argc, argv);
 
-    qmlRegisterType<valcapt>("VAL.CAPT",1,0,"VALCAPT");
+        qmlRegisterType<valcapt>("VAL.CAPT",1,0,"VALCAPT");
 
-    QQmlApplicationEngine engine;
+        QQmlApplicationEngine engine;
 
-    QQmlComponent component(&engine, QUrl(QLatin1String("qrc:/main.qml")));
+        QQmlComponent component(&engine, QUrl(QLatin1String("qrc:/main.qml")));
 
-    component.create();
+        component.create();
 
-    return app.exec();
+        return app.exec();
+
+//    valmoy val;
+//    int vitesse;
+//    int distance;
+//    QTextStream s(stdin);
+
+//    while(1)
+//    {
+//        qDebug()<<"vitesse";
+//        vitesse = s.readLine().toInt();
+//        qDebug()<<"distance";
+//        distance = s.readLine().toInt();
+
+//        val.addData(vitesse,distance);
+
+
+//    }
 }
